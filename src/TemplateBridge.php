7eaf8ab70784e7331bd15a0b00ce7883002ba749
yutaf <yutafuji2008@gmail.com>
2015-09-28 11:49:47 +0900

    Initial commit

---
+++ src/TemplateBridge.php
@@ -0,0 +1,310 @@
+<?php
+
+/**
+ * Class TemplateBridge
+ *
+ * @author Yuta Fujishiro<fujishiro@amaneku.co.jp>
+ */
+abstract class TemplateBridge
+{
+    protected $tpl;
+    protected $invoker;
+    protected $variables;
+    protected $base_dir;
+    protected $css = array();
+    protected $js = array();
+
+    abstract protected function getDefaultTemplate();
+    abstract protected function setGlobalVariables();
+    abstract protected function setBlocks();
+    protected function init()
+    {
+        // You can override in inherited class
+    }
+    
+    /**
+     * コンストラクタ
+     *
+     * @param HTML_Template_IT $tpl
+     * @param $invoker
+     * @param array $variables
+     */
+    public function __construct(HTML_Template_IT $tpl, $invoker, $variables=array())
+    {
+        $this->tpl = $tpl;
+        $this->invoker = $invoker;
+        $this->variables = $variables;
+        $this->base_dir = $this->getBaseDir();
+    }
+
+    /**
+     * テンプレート ベース ディレクトリを取得
+     *
+     * @return string
+     */
+    protected function getBaseDir()
+    {
+        switch(true) {
+            //TODO 定義していく
+            //case (strpos($this->invoker, '/admin/') !== false):
+            //    $base_dir = TEMPLATE_DIR.'admin/';
+            //    break;
+            default:
+                $script_dirname = dirname($_SERVER['SCRIPT_NAME']);
+                $dir_name_following_root_path = ($script_dirname == '/')? '' : substr($script_dirname, 1).'/';
+
+                $base_dir = TEMPLATE_DIR.$dir_name_following_root_path;
+        }
+        return ($this->isSpDir())? $base_dir.'sp/' : $base_dir;
+    }
+
+    /**
+     * render
+     *
+     * @param null $template
+     */
+    public function render($template=null)
+    {
+        $this->prepare($template);
+        $this->show();
+    }
+
+    /**
+     * テンプレートhtmlを文字列で取得
+     *
+     * @param null $template
+     * @return mixed
+     */
+    public function store($template=null)
+    {
+        $this->prepare($template);
+        return $this->get();
+    }
+
+    /**
+     * prepare
+     *
+     * @param $template
+     * @throws LogicException
+     */
+    protected function prepare($template)
+    {
+        if(strlen($template) == 0) {
+            $template = $this->getDefaultTemplate();
+        }
+        if(! is_file($template)) {
+            throw new LogicException('Indicated template does not exist. name: '.$template);
+        }
+        $result = $this->loadTemplatefile($template);
+        if(! $result) {
+            throw new LogicException('Failed to load: '.$template);
+        }
+        $this->init();
+        if(count($this->css) > 0) {
+            $this->parseCss();
+        }
+        if(count($this->js) > 0) {
+            $this->parseJs();
+        }
+        $this->setBlocks();
+        $this->setGlobalBlock();
+    }
+
+    /**
+     * set global block
+     */
+    protected function setGlobalBlock()
+    {
+        $this->setCurrentBlock();
+        $this->setGlobalVariables();
+        $this->parseCurrentBlock();
+    }
+
+    /**
+     * css をパース
+     */
+    protected function parseCss()
+    {
+        if(count($this->css) < 1) return;
+        foreach($this->css as $css) {
+            $this->setCurrentBlock('css');
+            $this->setVariable('css', $css);
+            $this->parseCurrentBlock();
+        }
+    }
+
+    /**
+     * js をパース
+     */
+    protected function parseJs()
+    {
+        if(count($this->js) < 1) return;
+        foreach($this->js as $js) {
+            $this->setCurrentBlock('js');
+            $this->setVariable('js', $js);
+            $this->parseCurrentBlock();
+        }
+    }
+
+    /**
+     * cssファイルのlinkタグを追加する
+     *
+     * @param array $css
+     */
+    public function addCss($css=array())
+    {
+        if(! is_array($css)) $css = array($css);
+        $this->css = array_merge($this->css, $css);
+    }
+
+    /**
+     * jsを追加する
+     *
+     * @param array $js
+     */
+    public function addJs($js=array())
+    {
+        if(! is_array($js)) $js = array($js);
+        $this->js = array_merge($this->js, $js);
+    }
+    
+    /**
+     * 呼び出し元のプログラムと同じパス構成を持つテンプレートを取得
+     *
+     * @return string
+     */
+    protected function getOwnTemplate()
+    {
+        // sp
+        if($this->isSpDir()){
+            return $this->getOwnTemplateSp();
+        }
+        // pc
+        return TEMPLATE_DIR.substr(str_replace('.php', '.html', $_SERVER['SCRIPT_NAME']), 1);
+    }
+
+    /**
+     * if you are in /sp directory
+     *
+     * @return bool
+     */
+    protected function isSpDir()
+    {
+        if(strpos($this->invoker, BASEDIR.'htdocs/sp/') === false) return false;
+        return true;
+    }
+
+    /**
+     * 呼び出し元のプログラムと同じパス構成を持つテンプレートを取得(スマホ用)
+     *
+     * @return string
+     */
+    private function getOwnTemplateSp()
+    {
+        $file = substr(str_replace('.php', '.html', $_SERVER['SCRIPT_NAME']), 1);
+
+        $pieces = explode('/', $file);
+        $last_key = end(array_keys($pieces));
+        $template_dir_sp = 'sp';
+        array_splice($pieces, $last_key, 0, $template_dir_sp);
+
+        $file = implode('/', $pieces);
+        return DIR_TEMPLATE.$file;
+    }
+    
+    /**
+     * 文字列をエスケープ
+     *
+     * @param $str
+     * @return string
+     * @throws LogicException
+     */
+    public function escape($str)
+    {
+        if(! defined('ENCODING_HTML_ESCAPE')) {
+            throw new LogicException('ERROR: ENCODING_HTML_ESCAPE is not defined.');
+        }
+        return htmlspecialchars($str, ENT_QUOTES, ENCODING_HTML_ESCAPE);
+    }
+
+    /**
+     * HTML_Template_IT::loadTemplatefile のラッパーメソッド
+     *
+     * @param $filename
+     * @param bool $removeUnknownVariables
+     * @param bool $removeEmptyBlocks
+     * @return mixed
+     */
+    protected function loadTemplatefile($filename, $removeUnknownVariables=true, $removeEmptyBlocks=true)
+    {
+        return $this->tpl->loadTemplatefile($filename, $removeUnknownVariables, $removeEmptyBlocks);
+    }
+
+    /**
+     * HTML_Template_IT::parse のラッパーメソッド
+     *
+     * @param $block
+     */
+    protected function parse($block)
+    {
+        $this->tpl->parse($block);
+    }
+
+    /**
+     * HTML_Template_IT::parseCurrentBlock のラッパーメソッド
+     */
+    protected function parseCurrentBlock()
+    {
+        $this->tpl->parseCurrentBlock();
+    }
+
+    /**
+     * HTML_Template_IT::setCurrentBlock のラッパーメソッド
+     *
+     * @param string $block
+     */
+    protected function setCurrentBlock($block = "__global__")
+    {
+        $this->tpl->setCurrentBlock($block);
+    }
+
+    /**
+     * HTML_Template_IT::setVariable のラッパーメソッド
+     *
+     * @param $placeholder
+     * @param $variable
+     */
+    protected function setVariable($placeholder, $variable)
+    {
+        $this->tpl->setVariable($placeholder, $variable);
+    }
+
+    /**
+     * HTML_Template_IT::show のラッパーメソッド
+     */
+    protected function show()
+    {
+        $this->tpl->show();
+    }
+
+    /**
+     * HTML_Template_IT::touchBlock のラッパーメソッド
+     *
+     * @param $block
+     */
+    protected function touchBlock($block='__global__')
+    {
+        $this->tpl->touchBlock($block);
+    }
+
+    /**
+     * HTML_Template_IT::get() のラッパーメソッド
+     *
+     * @param string $block
+     * @return string
+     */
+    protected function get($block = "__global__")
+    {
+        return $this->tpl->get($block);
+    }
+}
---
+++ src/TemplateManager.php
@@ -0,0 +1,208 @@
+<?php
+
+/**
+ * Class TemplateManager
+ *
+ * @author Yuta Fujishiro<fujishiro@amaneku.co.jp>
+ */
+class TemplateManager
+{
+    protected $tpl;
+    protected $invoker;
+    protected $variables;
+    protected $bridges = array();
+    protected $commonBridgePrefix;
+
+    /**
+     * コンストラクタ
+     *
+     * @param $tpl
+     * @param array $variables
+     */
+    public function __construct(HTML_Template_IT $tpl, $variables=array())
+    {
+        // デリミタの設定
+        $tpl->openingDelimiter='{{';
+        $tpl->closingDelimiter='}}';
+        // インスタンス代入
+        $this->tpl = $tpl;
+
+        $this->invoker = $this->getInvoker();
+        $defaults = (is_array($_SESSION))? $_SESSION : array();
+        $this->variables = array_merge($defaults, $variables);
+        $this->commonBridgePrefix = $this->getCommonBridgePrefix();
+    }
+
+    /**
+     * set variables
+     *
+     * @param $variables
+     */
+    public function setVariables($variables)
+    {
+        $this->variables = $variables;
+    }
+
+    /**
+     * add variables
+     *
+     * @param $variables
+     */
+    public function addVariables($variables)
+    {
+        $this->variables = array_merge($this->variables, $variables);
+    }
+
+    /**
+     * ブリッジ インスタンスを取得
+     *
+     * @param string $bridge_name
+     * @return mixed
+     */
+    public function get($bridge_name='')
+    {
+        if (!isset($this->bridges[$bridge_name])) {
+            $bridge_class = $bridge_name . 'Bridge';
+            $bridge = new $bridge_class($this->tpl, $this->invoker, $this->variables);
+
+            $this->bridges[$bridge_name] = $bridge;
+        }
+
+        return $this->bridges[$bridge_name];
+    }
+
+    /**
+     * プログラムのパス構成と同名のブリッジを取得
+     *
+     * @return string
+     */
+    public function getOwnBridgeName()
+    {
+        $str = str_replace('.php', '', substr($_SERVER['SCRIPT_NAME'], 1));
+        return $this->getBridgeStr($str);
+    }
+
+    /**
+     * Base ブリッジを返す
+     *
+     * @return string
+     */
+    public function getBaseBridgeName()
+    {
+        return $this->commonBridgePrefix.'Base';
+    }
+
+    /**
+     * Menu ブリッジを返す
+     *
+     * @return string
+     */
+    public function getMenuBridgeName()
+    {
+        return $this->commonBridgePrefix.'Menu';
+    }
+
+    /**
+     * Validate ブリッジを返す
+     *
+     * @return string
+     */
+    public function getValidateBridgeName()
+    {
+        return $this->commonBridgePrefix.'Validate';
+    }
+
+    /**
+     * Paging ブリッジを返す
+     *
+     * @return string
+     */
+    public function getPagingBridgeName()
+    {
+        return $this->commonBridgePrefix.'Paging';
+    }
+
+    /**
+     * Error ブリッジを返す
+     *
+     * @return string
+     */
+    public function getErrorBridgeName()
+    {
+        return $this->commonBridgePrefix.'Error';
+    }
+
+    /**
+     * 共通のブリッジ接頭句を返す
+     *
+     * @return string
+     * @throws LogicException
+     */
+    protected function getCommonBridgePrefix()
+    {
+        $paths = array(
+            '/admin/'
+        );
+        foreach($paths as $path) {
+            if($this->hasStr($this->invoker, $path)) return $this->getBridgeStr($path);
+        }
+        return '';
+    }
+
+    /**
+     * メソッドの呼び出し元ファイル名を取得
+     *
+     * @return mixed
+     */
+    protected function getInvoker()
+    {
+        $backtrace = debug_backtrace();
+        $last = end($backtrace);
+        return $last['file'];
+    }
+
+    /**
+     * 指定文字列を持つか判定
+     *
+     * @param $haystack
+     * @param $str
+     * @return bool
+     */
+    protected function hasStr($haystack, $str)
+    {
+        if(strpos($haystack, $str) !== false) return true;
+        return false;
+    }
+
+    /**
+     * ブリッジ用の文字列を返す
+     *
+     * @param $path
+     * @return string
+     */
+    protected function getBridgeStr($path)
+    {
+        if(substr($path, 0, 1) == '/') $path = substr($path, 1);
+        if(substr($path, -1) == '/') $path = rtrim($path, '/');
+        $pieces = explode('/', $path);
+        $str = '';
+        foreach($pieces as $piece) {
+            $str .= ucfirst($piece);
+        }
+        
+        return $str;
+    }
+
+    /**
+     * 複数のテンプレート・ブリッジをデフォルトテンプレートでレンダリング
+     *
+     * @param array $bridges
+     */
+    public function renderBridges($bridges=array())
+    {
+        if(count($bridges) == 0) return;
+        foreach($bridges as $bridge) {
+            if(strlen($bridge) > 0) $this->get($bridge)->render();
+        }
+    }
+}
